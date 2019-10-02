d259937cb536f64b7ec58744fea74fdf9441e0c4
Bogdan Nourescu <bogdan.nourescu@cbn-it.ro>
2019-10-02 15:42:24 +0300

    stringify the date correctly

--- utils/Utils.js
+++ utils/Utils.js
@@ -209,12 +209,14 @@ const _stringify = function(data, indent) {
 
         str  = indent;
 
+        str += '"';
         str += data.getUTCFullYear()                + '-';
         str += _format_date(data.getUTCMonth() + 1) + '-';
         str += _format_date(data.getUTCDate())      + 'T';
         str += _format_date(data.getUTCHours())     + ':';
         str += _format_date(data.getUTCMinutes())   + ':';
         str += _format_date(data.getUTCSeconds())   + 'Z';
+        str += '"';
 
     } else if (data instanceof Object) {
 
