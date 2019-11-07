a11f4f80e7113b1d445f8657055ae7ee7546cdca
Bogdan Nourescu <bogdan.nourescu@cbn-it.ro>
2019-11-07 12:17:45 +0200

    generic send function

--- mail/SendGridMail.js
+++ mail/SendGridMail.js
@@ -4,19 +4,15 @@ sgMail.setApiKey("SG.G0ASk_u-SHy4nRMKmS2WHg.B5_CxfGCNCZBvpIO4AbcGYJngARkIbskjTbR
 class SendGridMail{
 
     static async sendHtmlMail(to, from, subject, html){
-        const msg = {
-            to, from, subject, html
-        };
-        return await sgMail.send(msg);
+        return this.send({to, from, subject, html});
     }
 
     static async sendTextMail(to, from, subject, text){
-        const msg = {
-            to, from, subject, text
-        };
-        return await sgMail.send(msg);
+        return this.send({to, from, subject, text});
     }
-
+    static async send({to, from, replyTo, cc, bcc, sendAt, subject, text, html, content,attachments}){
+        return sgMail.send({to, from, replyTo, cc, bcc, sendAt, subject, text, html, content,attachments});
+    }
 }
 
 module.exports = SendGridMail;
