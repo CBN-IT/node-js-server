3e6bdab4534dfd7c4d65aaa42d5c4ab83f6514a5
Bogdan Nourescu <bogdan.nourescu@cbn-it.ro>
2020-04-07 01:42:41 +0300

    createSession didn't return anything

--- auth/CreateSession.js
+++ auth/CreateSession.js
@@ -1,5 +1,6 @@
 const Servlet = require('./../utils/Servlet');
 const admin = require('firebase-admin');
+const {AuthenticationError} = require("../utils/errors");
 
 class CreateSession extends Servlet {
     static url = '/CreateSession';
@@ -10,29 +11,24 @@ class CreateSession extends Servlet {
     async execute() {
         if (this.req.param.csrfToken !== this.req.cookies.csrfToken) {
             this.logger.w(this.req.param.csrfToken, this.req.cookies.csrfToken);
-            this.res.status(401).send('UNAUTHORIZED REQUEST!');
-            return;
+            throw new AuthenticationError("csrfToken from param does not match the value in cookies");
         }
         const expiresIn = 14 * 24 * 60 * 60 * 1000;
         // Create the session cookie. This will also verify the ID token in the process.
         // The session cookie will have the same claims as the ID token.
         // To only allow session cookie setting on recent sign-in, auth_time in ID token
         // can be checked to ensure user was recently signed in before creating a session cookie.
-        admin.auth().createSessionCookie(this.req.param.idToken, {expiresIn})
-            .then((sessionCookie) => {
-                // Set cookie policy for session cookie.
-                const options = {
-                    maxAge: expiresIn,
-                    httpOnly: true,
-                    secure: this.req.protocol === "https",
-                    sameSite: "lax"
-                };
-                this.res.cookie('session', sessionCookie, options);
-                this.res.clearCookie('csrfToken', options);
-                this.res.end(JSON.stringify({status: 'success'}));
-            }, error => {
-                this.res.status(401).send('UNAUTHORIZED REQUEST!');
-            });
+        let sessionCookie = admin.auth().createSessionCookie(this.req.param.idToken, {expiresIn});
+        // Set cookie policy for session cookie.
+        const options = {
+            maxAge: expiresIn,
+            httpOnly: true,
+            secure: this.req.protocol === "https",
+            sameSite: "lax"
+        };
+        this.res.cookie('session', sessionCookie, options);
+        this.res.clearCookie('csrfToken', options);
+        return {status: 'success'};
     }
 }
 
