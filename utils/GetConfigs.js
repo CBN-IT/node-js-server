b6168280c324dfcca66962bb2041339fe375d151
Bogdan Nourescu <bogdan.nourescu@cbn-it.ro>
2020-04-28 13:06:45 +0300

    moved the functions from getConfigs to saveForm

--- utils/GetConfigs.js
+++ utils/GetConfigs.js
@@ -1,53 +1,10 @@
-const Servlet = require('./Servlet.js');
-const fs = require("fs");
+const SaveForm = require('./SaveForm.js');
 
 /**
  * @abstract
  */
-class GetConfigs extends Servlet {
+class GetConfigs extends SaveForm {
 
-    async _getForms(folderName, collection, _companyId, justJSON){
-        let formsFromFile = this._getConfigs(folderName);
-        let formsDefault = await this.runQuery('', collection, []);
-        let forms = [];
-        if(_companyId !== 'default'){
-            forms = await this.runQuery(_companyId, collection, []);
-        }
-        return this._mergeForms(formsFromFile, formsDefault, forms, justJSON);
-    }
-
-    _getConfigs(folderName) {
-        let configs = {};
-        let folderPath = global.projectRoot + `${folderName}`;
-        fs.readdirSync(folderPath).forEach(file => {
-            if (file.split('.')[1] === "json") {
-                configs[file.split('.')[0]] = new Function("return " + fs.readFileSync(`${folderPath}/${file}`, 'utf8'))();
-            }
-        });
-        return configs;
-    }
-
-    _mergeForms(formsFromFile, formsDefault, forms, justJSON){
-        Object.keys(formsFromFile).forEach(key => {
-            formsFromFile[key] = justJSON ?
-                formsFromFile[key] :
-                {
-                    code: formsFromFile[key],
-                    collection: key,
-                    _id:key
-                }
-        });
-
-
-        formsDefault.forEach(form => {
-            formsFromFile[form.collection] = justJSON ? JSON.parse(form.code) : form;
-        });
-        forms.forEach(form => {
-            formsFromFile[form.collection] = justJSON ? JSON.parse(form.code) : form;
-        });
-
-        return formsFromFile;
-    }
 }
 
 module.exports = GetConfigs;
--- utils/SaveForm.js
+++ utils/SaveForm.js
@@ -152,6 +152,49 @@ class SaveForm extends Servlet {
         this.config = config;
     }
 
+    async _getForms(folderName, collection, _companyId, justJSON){
+        let formsFromFile = this._getConfigs(folderName);
+        let formsDefault = await this.runQuery('', collection, []);
+        let forms = [];
+        if(_companyId !== 'default'){
+            forms = await this.runQuery(_companyId, collection, []);
+        }
+        return this._mergeForms(formsFromFile, formsDefault, forms, justJSON);
+    }
+
+    _getConfigs(folderName) {
+        let configs = {};
+        let folderPath = global.projectRoot + `${folderName}`;
+        fs.readdirSync(folderPath).forEach(file => {
+            if (file.split('.')[1] === "json") {
+                configs[file.split('.')[0]] = new Function("return " + fs.readFileSync(`${folderPath}/${file}`, 'utf8'))();
+            }
+        });
+        return configs;
+    }
+
+    _mergeForms(formsFromFile, formsDefault, forms, justJSON){
+        Object.keys(formsFromFile).forEach(key => {
+            formsFromFile[key] = justJSON ?
+                formsFromFile[key] :
+                {
+                    code: formsFromFile[key],
+                    collection: key,
+                    _id:key
+                }
+        });
+
+
+        formsDefault.forEach(form => {
+            formsFromFile[form.collection] = justJSON ? JSON.parse(form.code) : form;
+        });
+        forms.forEach(form => {
+            formsFromFile[form.collection] = justJSON ? JSON.parse(form.code) : form;
+        });
+
+        return formsFromFile;
+    }
+
 }
 
 
