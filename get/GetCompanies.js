8c712ad269a9c566d06879417fffd000029e1a81
Bogdan Nourescu <bogdan.nourescu@cbn-it.ro>
2020-04-28 11:43:26 +0300

    if user is not SuperAdmin only give him his company.

--- get/GetCompanies.js
+++ get/GetCompanies.js
@@ -4,8 +4,13 @@ class GetCompanies extends Servlet {
 
     static url = '/GetCompanies';
 
-    async execute(){
-        return await this.runQuery('', 'company', [['_deleted', '==', null]]);
+    async execute() {
+        let account = await this.getAccount(this._companyId);
+        if (account.accountType === "SuperAdmin") {
+            return await this.runQuery('', 'company', [['_deleted', '==', null]]);
+        } else {
+            return [await this.getDocument('', 'company', this._companyId)]
+        }
     }
 }
 
