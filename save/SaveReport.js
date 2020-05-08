3851a9322199e23eafd288f7bcb7d946483d29bd
Bogdan Nourescu <bogdan.nourescu@cbn-it.ro>
2020-05-08 17:53:14 +0300

    added importTemplates to reports

--- save/SaveReport.js
+++ save/SaveReport.js
@@ -7,6 +7,7 @@ class SaveReport extends Servlet {
         let collection = 'report';
         let _id = this.req.param['_id'];
         let _companyId = this.req.param['_companyId'];
+        let importTemplates =this.req.param['importTemplates'] ? this.req.param['importTemplates'] instanceof Array ? this.req.param['importTemplates'] : [this.req.param['importTemplates']] : [];
         let newData = {
             'reportName': this.req.param['reportName'],
             'type': this.req.param['type'],
@@ -14,6 +15,7 @@ class SaveReport extends Servlet {
             'collection': this.req.param['collection'],
             'code': this.req.param['code'],
             'params': this.req.param['params'],
+            'importTemplates': importTemplates,
             '_deleted': null
         };
         return await this.updateDocument(_companyId, collection, _id, newData);
