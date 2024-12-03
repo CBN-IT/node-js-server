281025813498e1cf350530a4eba7f0ed77f5ce2a
Bogdan Nourescu <bogdan.nourescu@cbn-it.ro>
2024-12-03 10:54:28 +0200

    use the noXSSCheck params for reports

--- save/SaveReport.js
+++ save/SaveReport.js
@@ -1,4 +1,5 @@
 const Servlet = require('./../utils/Servlet');
+const getAsList = require("../utils/getAsList");
 class SaveReport extends Servlet {
 
     static url = '/SaveReport';
@@ -7,16 +8,16 @@ class SaveReport extends Servlet {
         let collection = 'report';
         let _id = this.req.param['_id'];
         let _companyId = this.req.param['_companyId'];
-        let importTemplates =this.req.param['importTemplates'] ? this.req.param['importTemplates'] instanceof Array ? this.req.param['importTemplates'] : [this.req.param['importTemplates']] : [];
+
         let newData = {
             'reportName': this.req.param['reportName'],
-            'groupName': this.req.param['groupName']||"",
+            'groupName': this.req.param['groupName'] || "",
             'type': this.req.param['type'],
             'generator': this.req.param['generator'],
             'collection': this.req.param['collection'],
-            'code': this.req.param['code'],
-            'params': this.req.param['params'],
-            'importTemplates': importTemplates,
+            'code': this.req.paramNoXSSCheck['code'],
+            'params': this.req.paramNoXSSCheck['params'],
+            'importTemplates': getAsList(this.req.param['importTemplates']),
             '_deleted': null
         };
         return await this.updateDocument(_companyId, collection, _id, newData);
